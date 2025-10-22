9c8fe608127f0738bf5a2bd05d16af86a6e8eff9
Ayush Munjal <ayushmunjal17@gmail.com>
2025-10-22 17:45:12 +0530

    Remove test files

--- wrong_solution_max_array.py
+++
@@ -1,9 +0,0 @@
-# Wrong Solution - This should FAIL some test cases
-# (Returns first element instead of maximum)
-
-# Read input
-n = int(input())
-arr = list(map(int, input().split()))
-
-# Wrong logic - just return first element
-print(arr[0])
